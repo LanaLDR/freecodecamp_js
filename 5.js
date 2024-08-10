d0af93229a9e02493b9abd573c48dc592077bdcc
Kirill Semin <sm.ksyomin@yandex.ru>
2024-08-11 02:40:02 +0300

    fix repet with 0

--- 5.js
+++ 5.js
@@ -2,8 +2,8 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i));
+for (let i = 0; i <= count; i = i + 1) {
+  rows.push(character.repeat(i+1))
 }
 
 let result = ""
