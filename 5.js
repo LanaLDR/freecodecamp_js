a153a64afd66e2a9bcdd57a5d505b93df10a8029
Kirill Semin <ksyomin.sm@gmail.com>
2024-08-13 02:06:38 +0300

    add if else block

--- 5.js
+++ 5.js
@@ -6,8 +6,17 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if ("") {
+  console.log("Condition is true");
+} else if (5 > 10) {
+  console.log("5 is less than 10");
+} else {
+  console.log("This is the else block");
 }
 
 let result = ""
