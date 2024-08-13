5cdcbf4980bef907aef80013b307472fb4761102
Kirill Semin <ksyomin.sm@gmail.com>
2024-08-13 20:17:30 +0300

    add !== in while

--- 5.js
+++ 5.js
@@ -11,12 +11,12 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(i, count));
 }*/
 
-if ("") {
-  console.log("Condition is true");
-} else if (5 > 10) {
-  console.log("5 is less than 10");
-} else {
-  console.log("This is the else block");
+let continueLoop = false;
+let done = 0;
+
+while (done !== count) {
+  done++;
+  rows.push(padRow(done, count));
 }
 
 let result = ""
