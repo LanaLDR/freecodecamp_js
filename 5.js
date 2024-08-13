fb23106f88706d7f61370b2d634920d9a379badb
Kirill Semin <ksyomin.sm@gmail.com>
2024-08-13 21:02:58 +0300

    add inverted var

--- 5.js
+++ 5.js
@@ -1,28 +1,19 @@
 const character = "#";
 const count = 8;
 const rows = [];
+let inverted = true;
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
-
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
-
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
-
-const numbers = [1, 2, 3];
-const unshifted = numbers.unshift(5);
-console.log(unshifted);
-console.log(numbers);
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
 
 let result = ""
 
